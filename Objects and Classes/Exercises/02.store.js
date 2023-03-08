2288a10e5410a49c0d03215b03721cadd6139693
Mariyana Pavlova <94930314+MariyanaPavlova@users.noreply.github.com>
2023-03-08 20:49:31 +0200

    update

--- Objects and Classes/Exercises/02.store.js
+++ Objects and Classes/Exercises/02.store.js
@@ -1,25 +1,24 @@
 function store(stock, delivery) {
+    
     let storeProd = {};
-    let stockLen = stock.length;
-    let deliveryLen = delivery.length;
 
-    for (let i = 0; i < stockLen; i += 2) {
+    for (let i = 0; i < stock.length; i += 2) {
         storeProd[stock[i]] = Number(stock[i + 1]);
     }
 
-    for (let i = 0; i < deliveryLen; i += 2) {
+    for (let i = 0; i < delivery.length; i += 2) {
         let prod = delivery[i];
 
         if (!storeProd.hasOwnProperty(prod)) {
         storeProd[prod] = 0;
         }    
         storeProd[prod] += Number(delivery[i + 1]);
-     
     }
 
-  for (const key in storeProd) {
-    console.log(`${key} -> ${storeProd[key]}`);
-  }
+    // for (let key of Object.keys(storeProd)) {
+    for (let key in storeProd) {
+        console.log(`${key} -> ${storeProd[key]}`);
+    }
 }
 
 store(
