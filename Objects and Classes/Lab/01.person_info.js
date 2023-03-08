6384c6fc277b421ebaeb52cdf9eda6cf8b84140f
Mariyana Pavlova <94930314+MariyanaPavlova@users.noreply.github.com>
2023-03-08 20:47:43 +0200

    exercises

--- Objects and Classes/Lab/01.person_info.js
+++ Objects and Classes/Lab/01.person_info.js
@@ -1,10 +1,14 @@
 function personInf(firstName, lastName, age) {
-    let res = {}
-    res.firstName = firstName
-    res.lastName = lastName
-    res.age = age
-    return res
+    // let person = {}
+    // res.firstName = firstName
+    // res.lastName = lastName
+    // res.age = age
+    age = Number(age)
+    let person = {firstName:firstName, lastName:lastName, age:age}
+    return person
 }
+    
+   
 
 console.log(personInf("Peter", "Pan", "20"))
 console.log(personInf("George", "Smith", "18"))
